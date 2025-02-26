0f8dccee27badac9f4c94f469720d40e829c18a4
EllenaAmanda <ellenaamanda846@gmail.com>
2025-02-27 01:59:17 +0700

    new photo for pp3t

--- src/pages/Porto2.jsx
+++ src/pages/Porto2.jsx
@@ -41,7 +41,7 @@ function Porto2() {
         <div className="px-[12%] py-[6%] bg-neutral-50 min-h-screen">
         <h2 className='font-[myFont] text-center text-3xl mb-[8%] '>Graphic Design</h2>
 
-          <div className='flex flex-col gap-24 items-center w-fit'> 
+          <div className='flex flex-col gap-8 md:gap-24 items-center w-fit'> 
 
           <div className="border border-neutral-200 h-80  overflow-x-auto overflow-y-hidden flex snap-x snap-mandatory rounded-md space-x-4">
               {first.map((url, index) => (
